1537863219499b89bec657f3f121cd5f7b0ad47b
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2023-04-20 02:18:57 +0200

    review is done

--- js/script.js
+++ js/script.js
@@ -1,88 +1,86 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        rtl: false,
-        loop: true,
-        margin: 10,
-        nav: true,
-        lazyLoad: true,
-        navText:['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 2
-            }
-        }
-    })
-});
-
-window.addEventListener("scroll",function(){
-    if (document.documentElement.scrollTop >200){
-        document.getElementById("home-but").style.display="block"
-    } else {
-        document.getElementById("home-but").style.display = "none"
-    }
+  $('.owl-carousel').owlCarousel({
+    rtl: false,
+    loop: true,
+    margin: 10,
+    nav: true,
+    lazyLoad: true,
+    navText: [
+      '<i class="fas fa-arrow-left"></i>',
+      '<i class="fas fa-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  })
 })
 
+window.addEventListener('scroll', function () {
+  if (document.documentElement.scrollTop > 200) {
+    document.getElementById('home-but').style.display = 'block'
+  } else {
+    document.getElementById('home-but').style.display = 'none'
+  }
+})
 
 // =========================================================
 
-
-
 var form = document.querySelector('#touch-form')
 
-form.addEventListener('input', function (e) {
-
+form &&
+  form.addEventListener('input', function (e) {
     switch (e.target.id) {
-        case 'name':
-            NameValidation(e.target)
-            break;
+      case 'name':
+        NameValidation(e.target)
+        break
 
-        case 'email':
-            emailValidation(e.target)
-            break;
-
+      case 'email':
+        emailValidation(e.target)
+        break
     }
-})
-
+  })
 
 function NameValidation(input) {
-    var inputValue = input.value.trim()
-    if (inputValue.length < 5) handleError(input, 'please enter at least 5 character')
-    else handleError(input)
+  var inputValue = input.value.trim()
+  if (inputValue.length < 5)
+    handleError(input, 'please enter at least 5 character')
+  else handleError(input)
 }
 
 function showError(element, msg) {
-    var errorElement = element.nextElementSibling
-    errorElement.innerText = msg
+  var errorElement = element.nextElementSibling
+  errorElement.innerText = msg
 }
 
 function removeError(element) {
-    var element = element.nextElementSibling
-    element.innerText = ""
+  var element = element.nextElementSibling
+  element.innerText = ''
 }
 
-
-function handleError(element, msg = "") {
-    var errorElement = element.nextElementSibling
-    errorElement.innerText = msg
+function handleError(element, msg = '') {
+  var errorElement = element.nextElementSibling
+  errorElement.innerText = msg
 }
-
 
 function emailValidation(input) {
-    var regExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    var inputValue = input.value
-    console.log(regExp.test(inputValue)); //return boolean type
+  var regExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+  var inputValue = input.value
+  console.log(regExp.test(inputValue)) //return boolean type
 
-    (regExp.test(inputValue) == false) ? handleError(input, "enter a valid email") : handleError(input)
-
+  regExp.test(inputValue) == false
+    ? handleError(input, 'enter a valid email')
+    : handleError(input)
 }
 
-
-form.addEventListener('submit', function (e) {
+form &&
+  form.addEventListener('submit', function (e) {
     console.log(e)
-})
+  })
